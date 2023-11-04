7f2cecd0af8be98fd8b59d2645cb9106e9520dae
BalajiPrasad Marni <marnibalajiprasad55@gmail.com>
2023-11-04 10:52:00 +0530

    Modified PatientModel

--- PatientManagement/PatientManagement/Model/Patient.cs
+++ PatientManagement/PatientManagement/Model/Patient.cs
@@ -14,35 +14,37 @@ namespace PatientManagement.Model
         [StringLength(50, ErrorMessage = "Last name must be between 1 and 50 characters.", MinimumLength = 1)]
         public string LastName { get; set; }
 
-        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Date of Birth is required.")]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        [Display(Name = "Date of Birth")]
         public DateTime DateOfBirth { get; set; }
 
         [Required(ErrorMessage = "Gender is required.")]
         public string Gender { get; set; }
 
-        [Phone(ErrorMessage = "Invalid contact number.")]
-        [Display(Name = "Contact Number")]
+        [Required(ErrorMessage = "ContactNumber is required.")]
         public string ContactNumber { get; set; }
 
+        [Required(ErrorMessage = "Weight is required.")]
         [Range(0, 1000, ErrorMessage = "Weight must be between 0 and 1000.")]
         public double Weight { get; set; }
 
+        [Required(ErrorMessage = "Height is required.")]
         [Range(0, 300, ErrorMessage = "Height must be between 0 and 300.")]
         public double Height { get; set; }
 
+        [Required(ErrorMessage = "Email is required.")]
         [EmailAddress(ErrorMessage = "Invalid email address.")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Address is required.")]
         [StringLength(100, ErrorMessage = "Address must be less than 100 characters.")]
         public string Address { get; set; }
 
+        [Required]
         [StringLength(500, ErrorMessage = "Medical comments must be less than 500 characters.")]
-        [Display(Name = "Medical Comments")]
         public string MedicalComments { get; set; }
 
-        [Display(Name = "Any Medications Taking")]
+        [Required(ErrorMessage = "AnyMedication Taking is required.")]
         public bool AnyMedicationsTaking { get; set; }
 
         [Required(ErrorMessage = "CreatedDate is required.")]
